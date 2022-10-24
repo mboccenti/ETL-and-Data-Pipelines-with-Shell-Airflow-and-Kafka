e84867b24273dd188833421869eea4dc216031a6
Marco Boccenti <marco.boccenti@gmail.com>
2022-10-24 14:11:23 +0200

    Update ETL_toll_data.py

--- ETL_toll_data.py
+++ ETL_toll_data.py
@@ -5,11 +5,10 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 
 # Task 1.1 - Define DAG arguments
-
 default_args = {
-    'owner':'Oguz Kedek',
+    'owner':'Marco Boccenti',
     'start_date':days_ago(0),
-    'email':['oguzkedek@gmail.com'],
+    'email':['marco.boccenti@gmail.com'],
     'email_on_failure':True,
     'email_on_retry':True,
     'retries':1,
@@ -17,7 +16,6 @@ default_args = {
 }
 
 # Task 1.2 - Define the DAG
-
 dag = DAG(
     'ETL_toll_data',
     default_args=default_args,
@@ -26,7 +24,6 @@ dag = DAG(
 )
 
 # Task 1.3 - Create a task to unzip data
-
 unzip_data = BashOperator(
     task_id = 'unzip_data',
     bash_command = 'tar -xzf /home/project/airflow/dags/finalassignment/tolldata.tgz',
@@ -34,7 +31,6 @@ unzip_data = BashOperator(
 )
 
 # Task 1.4 - Create a task to extract data from csv file
-
 extract_data_from_csv = BashOperator(
     task_id = 'extract_data_from_csv',
     bash_command = 'cut -d"," -f1-4 vehicle-data.csv > csv_data.csv',
@@ -42,7 +38,6 @@ extract_data_from_csv = BashOperator(
 )
 
 # Task 1.5 - Create a task to extract data from tsv file
-
 extract_data_from_tsv= BashOperator(
     task_id = 'extract_data_from_tsv',
     bash_command = 'cut -f5-7 tollplaza-data.tsv > tsv_data.csv',
@@ -50,7 +45,6 @@ extract_data_from_tsv= BashOperator(
 )
 
 # Task 1.6 - Create a task to extract data from fixed width file
-
 extract_data_from_fixed_width = BashOperator(
     task_id = 'extract_data_from_fixed_width',
     bash_command = 'awk 'NF{print $(NF-1),$NF}'  OFS="\t"  payment-data.txt > fixed_width_data.csv',
@@ -58,7 +52,6 @@ extract_data_from_fixed_width = BashOperator(
 )
 
 # Task 1.7 - Create a task to consolidate data extracted from previous tasks
-
 consolidate_data = BashOperator(
     task_id = 'consolidate_data',
     bash_command = 'paste csv_data.csv tsv_data.csv fixed_width_data.csv > extracted_data.csv',
